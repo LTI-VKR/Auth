667ac0690a62aa8885d35c4a6d53d4b1086eec0e
Aleksandr1331 <asf47@tpu.ru>
2026-05-11 23:57:42 +0700

    (Auth-1) Health check.
    add configuration and health check endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,59 @@
 package main
 
-func main() {
+import (
+	"auth/config"
+	api "auth/internal/api/http"
+	_ "auth/internal/api/http/docs"
+	"net/http"
+)
 
+// @title Auth API
+// @version 0.1.0
+// @description API сервиса аутентификации
+// @BasePath /
+func main() {
+	port := "6767"
+
+	_, err := config.NewConfig()
+	if err != nil {
+		panic(err)
+	}
+	//
+	//postgresPool, err := postgres.NewPool(cfg.DatabaseUrl)
+	//if err != nil {
+	//	panic("failed to create pool")
+	//}
+	//defer postgresPool.Close()
+	//
+	//MinioClient, err := minIO.NewMinioClient(cfg.Endpoint, cfg.Login, cfg.Password)
+	//if err != nil {
+	//	panic("failed to create minio client")
+	//}
+
+	// Репозитории
+	//cmdRepo := postgres.NewProfileCommandRepository(postgresPool)
+	//qryRepo := postgres.NewProfileQueryRepository(postgresPool)
+	//
+	//minioRepo := minIO.NewAvatarMinioRepository(MinioClient, cfg.Bucket)
+	//
+	//// Команды и Запросы
+	//createCmd := command.NewCreateProfileCommand(cmdRepo)
+	//updateCmd := command.NewUpdateProfileCommand(cmdRepo)
+	//getQry := query.NewGetProfileQuery(qryRepo)
+	//getAvatarUploadUrlCmd := command.NewGetAvatarQuery(minioRepo)
+	//getAvatarDownloadUrlCmd := query.NewGetAvatarQuery(minioRepo)
+	//
+	//// Хендлеры
+	//createHandler := handlers.NewCreateProfileHandler(createCmd)
+	//getHandler := handlers.NewGetProfileHandler(getQry)
+	//updateHandler := handlers.NewUpdateProfileHandler(updateCmd, getQry)
+	//GetAvatarUploadUrlHandler := handlers.NewGetAvatarUploadUrlHandler(getAvatarUploadUrlCmd)
+	//GetAvatarDownloadUrlHandler := handlers.NewGetAvatarDownloadUrlHandler(getAvatarDownloadUrlCmd)
+
+	// Роутер
+	router := api.NewRouter()
+
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		panic(err)
+	}
 }
--- internal/api/http/router.go
+++ internal/api/http/router.go
@@ -1,1 +1,26 @@
 package http
+
+import (
+	"auth/internal/api/http/handlers"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
+)
+
+func NewRouter() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(15 * time.Second))
+
+	r.Get("/health", handlers.Health)
+
+	r.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL("/swagger/doc.json")))
+	return r
+}
